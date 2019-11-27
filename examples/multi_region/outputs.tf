f08eb42ba9aaa792958f67e03c450f9d07bbde2d
Miles Matthias <mmatthias@google.com>
2019-11-27 14:59:56 -0700

    don't need this output in the multi-region example

--- examples/multi_region/outputs.tf
+++ examples/multi_region/outputs.tf
@@ -18,8 +18,3 @@ output "region_short_name_map" {
   description = "The 4 or 5 character shortname of any given region."
   value       = "${module.utils.region_short_name_map}"
 }
-
-output "region_short_name" {
-  description = "The 4 or 5 character shortname of a given region."
-  value       = "${module.utils.region_short_name}"
-}
